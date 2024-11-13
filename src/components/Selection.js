4f0c4dcbd7838fac76e48494ccc6cfb4ff83bc25
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-13 12:51:07 +0530

    passing handler prop

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,25 @@
+import React from 'react'
 
+const Selection = ({
+  applyColor,
+  updateSelectionStyle,
+  nextBackground,
+  selectionColors,
+  index,
+}) => {
+  const handleClick = () => {
+    applyColor(index, nextBackground)
+  }
+
+  return (
+    <div
+      className='fix-box'
+      style={{ backgroundColor: selectionColors[index] || 'transparent' }}
+      onClick={handleClick}
+    >
+      <h5>Selection</h5>
+    </div>
+  )
+}
+
+export default Selection
